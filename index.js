df5d7e4a23b2be49fba882d0d0686d8e8dc41670
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-03-28 12:08:48 -0400

    Passed all tests!

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name : "Emily Joy",
+    streetAddress : "6497 65th ST N"
+}
+
+//console.log(employee)
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...employee,
+        name : "Sam",
+        streetAddress : "11 Broadway"
+}}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee.name = "Sam",
+    employee.streetAddress = "12 Broadway"
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+     let newEmployee = { 
+        ...employee,}
+    delete newEmployee.name
+    return newEmployee
+                 
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name
+    return employee
+}
